e80b106cab9ead911a7bd64b8c816e62dbe34d6f
Unknown <rubengomezh17@gmail.com>
2018-05-23 08:33:34 +0200

    -ok

--- README.md
+++
@@ -1,1 +0,0 @@
-Curso-Angular-5
--- template/src/app/typescripts/pro/smoothscroll/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './mdb-page-scroll.directive';
-export * from './mdb-page-scroll.service';
-export * from './mdb-page-scroll.config';
-export * from './mdb-page-scroll.instance';
-export * from './mdb-page-scroll-util.service';
-
-export {SmoothscrollModule} from './mdb-page-scroll.module';
